dd613d6f7de5c912e9eaa5bf7b352221daad7fae
Ayush jha <jhaa97204@gmail.com>
2024-08-03 13:20:20 +0530

    new

---
+++ js lecture 43/index.js
@@ -0,0 +1,65 @@
+// // // // // function sayMyName() {
+// // // // //     console.log("ayush jha");
+// // // // // }
+
+// // // // // sayMyName();
+
+
+// // // // // function printCount(){
+// // // // //     for (let i = 0; i < 11; i++) {
+// // // // //         console.log(i);
+        
+// // // // //     }
+// // // // // }
+
+// // // // // printCount();
+
+// // // // // function printNumber(num){
+// // // // //     console.log(num);
+// // // // // }
+// // // // // printNumber(5);
+
+
+
+// // // // function getAverage(num1, num2){
+// // // //     let average = (num1 + num2) / 2;
+// // // //     console.log(average);
+// // // // }
+
+// // // // getAverage(2,4);
+
+
+
+// // // function getSum(a,b,c){
+// // //     let sum = a + b + c;
+// // //     // return sum;
+// // // }
+
+// // // let ans = getSum(1,2,3);
+// // // console.log(ans);
+
+
+// // function getMyName(firstName, lastName){
+// //     let fullName =  firstName+ " " + lastName;
+// //     return fullName;
+// // }
+
+// // let fullName = getMyName("ayush","jha");
+// // console.log(fullName);
+
+
+// let squareNumber = function(num){
+//     let ans = num**2;
+//     return ans;
+// }
+
+// let ans = squareNumber(5);
+// console.log(ans);
+
+
+let getExp = (x,y) => {
+    let ans = x**y;
+    return ans;
+}
+
+console.log(getExp(2,10));
