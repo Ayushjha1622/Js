691f6bce160debe1cb5cc4d2f65416fb402fda1d
Ayush jha <jhaa97204@gmail.com>
2024-09-03 15:19:42 +0530

    new file

---
+++ js_lecture_48/index.js
@@ -0,0 +1,46 @@
+
+
+
+// let curr = new Date();
+// let date = new Date('june 20 1998 07:55');
+// let newDate = new Date(1998,11,7);
+// console.log(newDate)
+// console.log(newDate.getMonth());
+
+
+function getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+  }
+  getRandomIntInclusive(1,10)
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // console.log("babbar");
+// console.log(Math.PI);
+// console.log(Math.max(34,45,6,7,894,32));
+// console.log(Math.min(34,45,6,7,894,32));
+// console.log(Math.round(1.4));
+// console.log(Math.floor(1.4));
+// console.log(Math.ceil(1.4));
+// console.log(Math.abs(-1.4));
+// console.log(Math.random());
+// console.log(Math.sqrt(5));
+
+
