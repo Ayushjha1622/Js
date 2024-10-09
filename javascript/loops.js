c11bee83ddaa5e144fca3aced3ccef1c9bc1e2c7
Ayush jha <jhaa97204@gmail.com>
2024-10-09 19:59:28 +0530

    [new file]

---
+++ javascript/loops.js
@@ -0,0 +1,32 @@
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     if ( element==5 ) {
+//         console.log("5");  
+//     }else{
+//         console.log(element);
+//     } 
+// }
+
+
+
+// for (let i = 0; i <= 10; i++) {
+//    console.log(`outer loop value: ${i}`);
+//    for (let j = 0; j <= 10; j++) {
+//     console.log(`inner loop value ${i}and inner loop ${i + '*' + j + ' = '  + i*j }`);
+    
+   
+//    }   
+// }
+
+// let myArray = ["flash", "batman", "superman"]
+
+// for (let i = 0; i < myArray.length; i++) {
+//     const element = myArray[i];
+//     console.log(element);
+    
+    
+// }
+
+
+
+
